373c67cd6a730ffa63b89ca7a9fc00caef75ee64
aditya01933 <adityatiwari01933@gmail.com>
2016-10-30 00:25:11 +0530

    initial app setup

---
+++ lib/dependencies.rb
@@ -0,0 +1,3 @@
+require_relative "nearby_locator"
+require_relative "nearby_locator/calculation"
+require_relative "nearby_locator/geocoder"
---
+++ lib/nearby_locator.rb
@@ -0,0 +1,5 @@
+require_relative "dependencies"
+
+module NearbyLocator
+
+end
